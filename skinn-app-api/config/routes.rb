07b4d92a07b5fc7f006d4fa3d56b446eb582b9a9
cbatsoula <cbatsoula@gmail.com>
2019-07-29 16:58:31 -0400

    midway, can post, update soon

--- skinn-app-api/config/routes.rb
+++ skinn-app-api/config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   get '/login', to: "users#login"
   post '/user_products/addProducts', to: "user_products#add_products"
+  patch '/reviews/:id', to: "reviews#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

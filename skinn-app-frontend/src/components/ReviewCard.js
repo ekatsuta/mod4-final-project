b566302b1d8c2ed697b02931c89ed40a17f4765a
github username <github email address>
2019-07-31 13:13:39 -0400

    bug fixed for quiz page

--- skinn-app-frontend/src/components/ReviewCard.js
+++ skinn-app-frontend/src/components/ReviewCard.js
@@ -5,8 +5,6 @@ class ReviewCard extends React.Component {
 
   renderUser = () => {
 
-    console.log("users", this.props.users)
-    debugger;
     if (this.props.review) {
       let foundUser = this.props.users.find(user => {
         return this.props.review.user_id === user.id
